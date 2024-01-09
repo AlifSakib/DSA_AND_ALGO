2ce05553f0d611ffb80096fc8888eded95e578f0
AlifSakib <alifsakib@gmail.com>
2024-01-09 23:46:48 +0600

    Pointe

---
+++ data-structures/List_List/intro.js
@@ -0,0 +1,34 @@
+// Author : Alifsakib
+
+// Threre are two types of list
+// 1. Singly linked list
+// 2. Doubly linked list
+
+// Singly linked list
+
+const basket = ['apples', 'grapes', 'pears'];
+
+//linked list : apples --> grapes --> pears
+
+// apples
+// 8947 --> grapes
+//           8742 --> pears
+//                     372 --> null
+
+
+// Doubly linked list
+
+// apples
+// 8947 <--> grapes
+//           8742 <--> pears
+
+
+// Why linked list?
+// 1. Arrays are ordered in memory
+// 2. Linked list are not ordered in memory
+
+// https://visualgo.net/en
+
+// The Main Difference Between Arrays and Linked Lists
+// 1. Arrays are index based
+// 2. Linked lists are not index based
