f83cc14d96dffe9232535ac38c7efbbe35338262
AlifSakib <alifsakib@gmail.com>
2024-01-10 22:14:09 +0600

    Added Node Append and Prepend

--- data-structures/List_List/pointer.js
+++ data-structures/List_List/pointer.js
@@ -24,6 +24,7 @@ class LinkedList {
         this.tail = this.head;
         this.length = 1;
     }
+
     append(value){
         const newNode = {
             value : value,
@@ -34,11 +35,23 @@ class LinkedList {
         this.length++;
         return this;
     }
+
+    prepend(value){
+        const newNode = {
+            value : value,
+            next: null,
+        }
+        newNode.next = this.head;
+        this.head = newNode;
+        this.length++;
+        return this;
+    }
 }
 
 const myLinkedList = new LinkedList(10);
 myLinkedList.append(5);
 myLinkedList.append(16)
+myLinkedList.prepend(1);
 
 console.log(myLinkedList);
 
