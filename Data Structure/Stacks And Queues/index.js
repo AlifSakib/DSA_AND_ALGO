3449e868511ccbef33134e0dfcf886ec209a3027
AlifSakib <alifsakib@gmail.com>
2024-01-16 00:14:39 +0600

    Stack and Queue

--- Data Structure/Stacks And Queues/index.js
+++ Data Structure/Stacks And Queues/index.js
@@ -1,3 +1,49 @@
 // Author : Alifsakib
 
 // Stack
+
+// Stacks are like plates stacked on top of each other. 
+// The last plate put on the stack is the first one to be removed. 
+// This is called LIFO (Last In First Out).
+
+// when to use stack
+
+// 1. When you need to reverse things
+// 2. When you need to keep track of the order of things
+// 3. When you need to do something in the reverse order
+
+// Example
+
+// undo/redo
+// call stack
+// routing (history object) in React
+// browser history
+// recursive functions
+// depth first search
+
+// Queue
+
+// Queues are like people standing in line to buy tickets.
+// The first person to stand in line is the first one to buy a ticket.
+// This is called FIFO (First In First Out).
+
+// when to use queue
+
+// 1. When you need things to be processed in order
+// 2. When you need to keep track of the order of things
+
+//  Example
+
+// printer
+// operating system
+// web server
+// file system
+
+// Why not use arrays for stacks and queues?
+
+// Arrays are great for stacks and queues, but they are not optimal.
+// The reason is that arrays are optimized for random access,
+// and not sequential access. In other words, arrays are great for
+
+
+
