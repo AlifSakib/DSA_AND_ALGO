6e5e8ee686a6e94cc623cfba20169d0306ce1129
AlifSakib <alifsakib@gmail.com>
2024-01-16 23:17:07 +0600

    Stack n Queue

---
+++ Data Structure/Stacks And Queues/exercise.js
@@ -0,0 +1,13 @@
+// Stacks
+
+// Should i use Array or Linked List?
+
+// Exercise: Implement a Stack using Linked List
+
+
+
+// Queues
+
+// Should i use Array or Linked List?
+
+// Exercise: Implement a Stack using Linked List
